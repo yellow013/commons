6ef2b9970b266293b53dcd7fdd1739b278a88b1e
yellow013 <wk9988@gmail.com>
2018-08-02 13:32:46 +0800

    update StringUtil

--- common/src/main/java/io/ffreedom/common/utils/StringUtil.java
+++ common/src/main/java/io/ffreedom/common/utils/StringUtil.java
@@ -8,9 +8,19 @@ public final class StringUtil {
 	public final static boolean isNullOrEmpty(String str) {
 		return str == null || str.isEmpty();
 	}
-	
+
 	public final static boolean isEquals(String str1, String str2) {
 		return str1.equals(str2);
+	}
+
+	public final static boolean isNumber(String str) {
+		char[] chars = str.toCharArray();
+		for (char c : chars) {
+			if (!Character.isDigit(c)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
