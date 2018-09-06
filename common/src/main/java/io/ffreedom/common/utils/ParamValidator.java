56155e1803ecd85f1d1e33876e32e5d0dba74bf5
yellow013 <wk9988@gmail.com>
2018-09-06 10:55:49 +0800

    update param validator

--- common/src/main/java/io/ffreedom/common/utils/ParamValidator.java
+++ common/src/main/java/io/ffreedom/common/utils/ParamValidator.java
@@ -5,20 +5,19 @@ public final class ParamValidator {
 	private ParamValidator() {
 	}
 
-	public final static boolean paramIsNull(Object objs) {
-		return objs == null;
+	public final static boolean isNull(Object obj) {
+		return obj == null;
 	}
 
-	public final static boolean paramIsNull(Object... objs) {
-		if (objs != null) {
-			for (Object obj : objs) {
-				if (obj == null) {
-					return true;
-				}
-			}
-			return false;
+	public final static boolean isNull(Object... objs) {
+		if (objs == null) {
+			return true;
 		}
-		return true;
+		for (Object obj : objs) {
+			if (obj == null)
+				return true;
+		}
+		return false;
 	}
 
 }
