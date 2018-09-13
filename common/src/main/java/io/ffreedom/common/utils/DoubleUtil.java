9dad60c6bfaadbfbcedf600cf599c0a578250f21
yellow013 <wk9988@gmail.com>
2018-09-13 13:32:16 +0800

    update double util

--- common/src/main/java/io/ffreedom/common/utils/DoubleUtil.java
+++ common/src/main/java/io/ffreedom/common/utils/DoubleUtil.java
@@ -1,32 +1,34 @@
 package io.ffreedom.common.utils;
 
+import java.math.MathContext;
+
 public final class DoubleUtil {
 
-	private final static long DOUBLE_TO_LONG_MULTIPLIER = 10_000_000_000L;
+	private final static long DOUBLE_TO_LONG_MULTIPLIER = 10_000_000_00L;
 
 	private final static long DOUBLE_TO_LONG_CORRECTION_FACTOR = 1L;
 
 	private final static long DOUBLE_TO_LONG_DIVISOR = 10L;
 
-	private final static double LONG_TO_DOUBLE_DIVISOR = 1_000_000_000D;
+	private final static double LONG_TO_DOUBLE_DIVISOR = 1_000_000_00D;
 
 	/**
-	 * 保留小数点后9位的精度
+	 * 保留小数点后8位的精度
 	 * 
 	 * @param d1
 	 * @return
 	 */
-	public static long doubleToLong(double d1) {
+	private static long doubleToLong(double d1) {
 		return ((long) (d1 * DOUBLE_TO_LONG_MULTIPLIER) + DOUBLE_TO_LONG_CORRECTION_FACTOR) / DOUBLE_TO_LONG_DIVISOR;
 	}
 
 	/**
-	 * 小数点左移9位
+	 * 小数点左移8位
 	 * 
 	 * @param l1
 	 * @return
 	 */
-	public static double longToDouble(long l1) {
+	private static double longToDouble(long l1) {
 		return l1 / LONG_TO_DOUBLE_DIVISOR;
 	}
 
@@ -39,13 +41,30 @@ public final class DoubleUtil {
 		return longToDouble(doubleToLong(d1));
 	}
 
-	public static void main(String[] args) {
+	public static double add(double d1, double d2) {
+		return correction(d1 + d2);
+	}
 
+	public static double subtraction(double d1, double d2) {
+		return correction(d1 - d2);
+	}
+
+	public static double multiply(double d1, double d2) {
+		return correction(d1 * d2);
+	}
+
+	public static double division(double d1, double d2) {
+		return correction(d1 / d2);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Math.PI);
 		double d = 1.0D;
 		for (;;) {
-			d = correction(d + 0.01);
+			d = multiply(d, 1.1);
 			System.out.println(d);
-			if (d > 3) {
+			if (d > 10) {
 				break;
 			}
 		}
