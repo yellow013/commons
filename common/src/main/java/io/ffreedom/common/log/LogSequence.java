da9bb00fb4331029b59e6eab932d7938bb1bd9bb
yellow013 <wk9988@gmail.com>
2018-10-08 15:39:45 +0800

    update pom.xml

--- common/src/main/java/io/ffreedom/common/log/LogSequence.java
+++ common/src/main/java/io/ffreedom/common/log/LogSequence.java
@@ -1,9 +1,25 @@
 package io.ffreedom.common.log;
 
-public class LogSequence {
+public final class LogSequence {
+
+	public static long sysMillisecond() {
+		return System.nanoTime() / 1000_000;
+	}
 
 	public static long sysMicrosecond() {
 		return System.nanoTime() / 1000;
+	}
+
+	public static long sysNanosecond() {
+		return System.nanoTime();
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(LogSequence.sysNanosecond());
+		System.out.println(LogSequence.sysMicrosecond());
+		System.out.println(LogSequence.sysMillisecond());
+
 	}
 
 }
