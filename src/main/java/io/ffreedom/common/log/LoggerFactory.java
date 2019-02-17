84fbc60d4106c2c92c7c8bc9ce105d1eae931318
yellow013 <wk9988@gmail.com>
2019-02-17 22:43:38 +0800

    update logger

--- src/main/java/io/ffreedom/common/log/LoggerFactory.java
+++ src/main/java/io/ffreedom/common/log/LoggerFactory.java
@@ -11,43 +11,34 @@ public class LoggerFactory {
 	public static Logger getLogger(Class<?> clazz) {
 		if (!LoggerSetter.isLogFolderSetted()) {
 			String logFolder = System.getProperty(LoggerConstant.LOG_FOLDER);
-			if (StringUtil.isNullOrEmpty(logFolder)) {
+			if (StringUtil.isNullOrEmpty(logFolder))
 				LoggerSetter.setLogFolder("default");
-			}
 		}
 		if (!LoggerSetter.isLogFilenameSetted()) {
 			String logFilename = System.getProperty(LoggerConstant.LOG_FILENAME);
-			if (StringUtil.isNullOrEmpty(logFilename)) {
+			if (StringUtil.isNullOrEmpty(logFilename))
 				LoggerSetter.setLogFileName("java.runtime");
-			}
 		}
 		if (!LoggerSetter.isLogLevelSetted()) {
 			String logLevel = System.getProperty(LoggerConstant.LOG_LEVEL);
-			if (StringUtil.isNullOrEmpty(logLevel)) {
+			if (StringUtil.isNullOrEmpty(logLevel))
 				LoggerSetter.setLogLevel(LogLevel.INFO);
-			}
 		}
 		return org.slf4j.LoggerFactory.getLogger(clazz);
 	}
 
 	public static void main(String[] args) {
-
 		System.out.println(System.getProperty("user.home"));
 
 		LoggerSetter.setLogFileName("new");
-
 		LoggerSetter.setLogLevel(LogLevel.INFO);
-
 		Logger logger = getLogger(LoggerFactory.class);
 
-		logger.info("777");
-
+		logger.warn("777");
 		logger.info("778");
-
 		logger.debug("779");
 
 		System.out.println(System.getProperty(LoggerConstant.LOG_FOLDER));
-
 		System.out.println(LocalDateTime.now());
 	}
 
