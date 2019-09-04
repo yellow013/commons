383c7db36f9a694a07c514f719493002adc8e2d6
yellow013 <wk9988@gmail.com>
2019-09-04 10:42:17 +0800

    update new set

--- commons-core/src/main/java/io/ffreedom/common/collections/set/DeRepeatCounterSet.java
+++ commons-core/src/main/java/io/ffreedom/common/collections/set/DeRepeatCounterSet.java
@@ -1,10 +1,31 @@
 package io.ffreedom.common.collections.set;
 
-import io.ffreedom.common.number.counters.Counter;
+import org.eclipse.collections.api.map.primitive.MutableIntObjectMap;
 
-public class CounterSet<T extends Counter<T>> {
+import io.ffreedom.common.collections.MutableMaps;
+import io.ffreedom.common.number.counters.SyncDeRepeatCounter;
 
-	
-	
-	
+public final class DeRepeatCounterSet<T> {
+
+	private SyncDeRepeatCounter<T> counter = new SyncDeRepeatCounter<>();
+	private MutableIntObjectMap<SyncDeRepeatCounter<T>> groupCounter = MutableMaps.newIntObjectHashMap();
+
+	public SyncDeRepeatCounter<T> getCounter() {
+		return counter;
+	}
+
+	public synchronized SyncDeRepeatCounter<T> getCounterByGroup(int groupId) {
+		SyncDeRepeatCounter<T> counter = groupCounter.get(groupId);
+		if (counter == null) {
+			counter = new SyncDeRepeatCounter<>();
+			groupCounter.put(groupId, counter);
+		}
+		return counter;
+	}
+
+	public void putEvent(int groupId, T event) {
+		counter.add(event);
+		getCounterByGroup(groupId).add(event);
+	}
+
 }
