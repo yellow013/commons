8243a2bea02133ea6ca7e5da45390b62e1c1e621
yellow013 <wk9988@gmail.com>
2019-09-11 11:43:38 +0800

    update join key map

--- commons-core/src/main/java/io/ffreedom/common/param/JointKeyParamMap.java
+++ commons-core/src/main/java/io/ffreedom/common/param/JointKeyParamMap.java
@@ -1,35 +1,62 @@
 package io.ffreedom.common.param;
 
-import java.time.temporal.Temporal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import org.eclipse.collections.api.map.primitive.MutableLongBooleanMap;
 import org.eclipse.collections.api.map.primitive.MutableLongDoubleMap;
-import org.eclipse.collections.api.map.primitive.MutableLongIntMap;
+import org.eclipse.collections.api.map.primitive.MutableLongLongMap;
 import org.eclipse.collections.api.map.primitive.MutableLongObjectMap;
 import org.eclipse.collections.impl.map.mutable.primitive.LongBooleanHashMap;
 import org.eclipse.collections.impl.map.mutable.primitive.LongDoubleHashMap;
-import org.eclipse.collections.impl.map.mutable.primitive.LongIntHashMap;
+import org.eclipse.collections.impl.map.mutable.primitive.LongLongHashMap;
 import org.eclipse.collections.impl.map.mutable.primitive.LongObjectHashMap;
 
-public abstract class JointKeyParamMap<K1 extends JointKey, K2 extends JointKey> {
+public abstract class JointKeyParamMap {
 
 	private MutableLongBooleanMap booleanParamMap = new LongBooleanHashMap();
-	private MutableLongIntMap integerParamMap = new LongIntHashMap();
+	private MutableLongLongMap longParamMap = new LongLongHashMap();
 	private MutableLongDoubleMap doubleParamMap = new LongDoubleHashMap();
 	private MutableLongObjectMap<String> stringParamMap = new LongObjectHashMap<>();
-	private MutableLongObjectMap<Temporal> temporalParamMap = new LongObjectHashMap<>();
+	private MutableLongObjectMap<LocalDate> localDateParamMap = new LongObjectHashMap<>();
+	private MutableLongObjectMap<LocalTime> localTimeParamMap = new LongObjectHashMap<>();
+	private MutableLongObjectMap<LocalDateTime> localDateTimeParamMap = new LongObjectHashMap<>();
 
 	public JointKeyParamMap() {
 
 	}
 
-	public void put(K1 key1, K2 key2, ParamType type, Object value) {
-
+	public void put(int key1, int key2, boolean param) {
+		booleanParamMap.put(JointIdUtil.jointId(key1, key2), param);
 	}
 
-	public boolean get(K1 key1, K2 key2) {
-		int key = key1.id() * 1000000000 + key2.id();
-		return booleanParamMap.get(key);
+	public void put(int key1, int key2, long param) {
+		longParamMap.put(JointIdUtil.jointId(key1, key2), param);
+	}
+
+	public void put(int key1, int key2, double param) {
+		doubleParamMap.put(JointIdUtil.jointId(key1, key2), param);
+	}
+
+	public void put(int key1, int key2, String param) {
+		stringParamMap.put(JointIdUtil.jointId(key1, key2), param);
+	}
+
+	public void put(int key1, int key2, LocalDate param) {
+		localDateParamMap.put(JointIdUtil.jointId(key1, key2), param);
+	}
+
+	public void put(int key1, int key2, LocalTime param) {
+		localTimeParamMap.put(JointIdUtil.jointId(key1, key2), param);
+	}
+
+	public void put(int key1, int key2, LocalDateTime param) {
+		localDateTimeParamMap.put(JointIdUtil.jointId(key1, key2), param);
+	}
+
+	public boolean getBoolean(int key1, int key2) {
+		return booleanParamMap.get(JointIdUtil.jointId(key1, key2));
 	}
 
 }
