5430f17cb38b239c25fed35d7725b9363ed34a50
yellow013 <wk9988@gmail.com>
2019-09-19 09:44:33 +0800

    update util

--- commons-core/src/main/java/io/ffreedom/common/utils/BinaryUtil.java
+++ commons-core/src/main/java/io/ffreedom/common/utils/BinaryUtil.java
@@ -4,8 +4,6 @@ public class BinaryUtil {
 
 	public static final int BYTE_BIT_SIZE = Byte.SIZE;
 
-	public static final int SHORT_BIT_SIZE = Short.SIZE;
-
 	public static final int CHAR_BIT_SIZE = Character.SIZE;
 
 	public static final int INT_BIT_SIZE = Integer.SIZE;
@@ -14,45 +12,46 @@ public class BinaryUtil {
 
 	public static String byteToBinaryString(byte b) {
 		String binaryString = Integer.toBinaryString(b);
-		int length0 = BYTE_BIT_SIZE - binaryString.length();
-		return highPositionFilling(binaryString, length0);
-	}
-
-	public static String shortToBinaryString(short s) {
-		String binaryString = Integer.toBinaryString(s);
-		int length0 = SHORT_BIT_SIZE - binaryString.length();
-		return highPositionFilling(binaryString, length0);
+		int length = BYTE_BIT_SIZE - binaryString.length();
+		return highPositionFilling(new StringBuilder(BYTE_BIT_SIZE), binaryString, length);
 	}
 
 	public static String charToBinaryString(char c) {
 		String binaryString = Integer.toBinaryString(c);
-		int length0 = CHAR_BIT_SIZE - binaryString.length();
-		return highPositionFilling(binaryString, length0);
+		int length = CHAR_BIT_SIZE - binaryString.length();
+		return highPositionFilling(new StringBuilder(CHAR_BIT_SIZE), binaryString, length);
 	}
 
 	public static String intToBinaryString(int i) {
 		String binaryString = Integer.toBinaryString(i);
-		int length0 = INT_BIT_SIZE - binaryString.length();
-		return highPositionFilling(binaryString, length0);
+		int length = INT_BIT_SIZE - binaryString.length();
+		return highPositionFilling(new StringBuilder(INT_BIT_SIZE), binaryString, length);
 	}
 
 	public static String longToBinaryString(long l) {
 		String binaryString = Long.toBinaryString(l);
-		int length0 = LONG_BIT_SIZE - binaryString.length();
-		return highPositionFilling(binaryString, length0);
+		int length = LONG_BIT_SIZE - binaryString.length();
+		return highPositionFilling(new StringBuilder(LONG_BIT_SIZE), binaryString, length);
 	}
 
-	private static String highPositionFilling(String binaryString, int i) {
-		for (; i > 0; i--)
-			binaryString = "0" + binaryString;
-		return binaryString;
+	private static String highPositionFilling(StringBuilder builder, String binaryValue, int length) {
+		builder.append(binaryValue);
+		for (; length > 0; length--)
+			builder.insert(0, '0');
+		return builder.toString();
 	}
 
 	public static void main(String[] args) {
+		byte b = 3;
 
-		System.out.println(BinaryUtil.longToBinaryString(3L));
+		System.out.println(BinaryUtil.longToBinaryString(-3L));
+		System.out.println(BinaryUtil.intToBinaryString(-3));
+		System.out.println(BinaryUtil.charToBinaryString('3'));
+		System.out.println(BinaryUtil.byteToBinaryString(b));
 		System.out.println(BinaryUtil.intToBinaryString(2));
+		System.out.println(Integer.toBinaryString(2));
 		System.out.println(BinaryUtil.intToBinaryString(-10));
+
 	}
 
 }
