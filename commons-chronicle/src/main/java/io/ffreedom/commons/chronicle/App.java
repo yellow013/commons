0aa8a5fc49b98b152a8152018351d3abb317ecb5
yellow013 <wk9988@gmail.com>
2019-09-30 09:40:43 +0800

    update new file

---
+++ commons-chronicle/src/main/java/io/ffreedom/commons/chronicle/App.java
@@ -0,0 +1,13 @@
+package io.ffreedom.commons.chronicle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
