8233786c4117fa1ea5692c3881c09ad1bca7eb8e
yellow013 <wk9988@gmail.com>
2019-10-22 14:00:00 +0800

    update accessor

--- commons-chronicle/src/main/java/io/ffreedom/commons/chronicle/queue/accessor/StringReader.java
+++ commons-chronicle/src/main/java/io/ffreedom/commons/chronicle/queue/accessor/StringReader.java
@@ -1,9 +1,12 @@
 package io.ffreedom.commons.chronicle.queue.accessor;
 
+import javax.annotation.concurrent.NotThreadSafe;
+
 import io.ffreedom.commons.chronicle.queue.base.DataReader;
 import io.ffreedom.commons.chronicle.queue.base.FileCycle;
 import net.openhft.chronicle.queue.ExcerptTailer;
 
+@NotThreadSafe
 public final class StringReader extends DataReader<String> {
 
 	private StringReader(ExcerptTailer tailer, FileCycle fileCycle) {
--- commons-chronicle/src/main/java/io/ffreedom/commons/chronicle/queue/accessor/StringWriter.java
+++ commons-chronicle/src/main/java/io/ffreedom/commons/chronicle/queue/accessor/StringWriter.java
@@ -1,8 +1,11 @@
 package io.ffreedom.commons.chronicle.queue.accessor;
 
+import javax.annotation.concurrent.NotThreadSafe;
+
 import io.ffreedom.commons.chronicle.queue.base.DataWriter;
 import net.openhft.chronicle.queue.ExcerptAppender;
 
+@NotThreadSafe
 public final class StringWriter extends DataWriter<String> {
 
 	private StringWriter(ExcerptAppender appender) {
