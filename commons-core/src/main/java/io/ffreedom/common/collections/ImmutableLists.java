649eb0703ca295c0b9b080ef29a0297c91526f52
yellow013 <wk9988@gmail.com>
2019-11-18 13:25:49 +0800

    update immutable list

--- commons-core/src/main/java/io/ffreedom/common/collections/ImmutableLists.java
+++ commons-core/src/main/java/io/ffreedom/common/collections/ImmutableLists.java
@@ -1,10 +1,14 @@
 package io.ffreedom.common.collections;
 
-import static org.eclipse.collections.impl.list.immutable.ImmutableListFactoryImpl.INSTANCE;
-
-import java.util.function.Supplier;
-
+import org.eclipse.collections.api.factory.list.ImmutableListFactory;
+import org.eclipse.collections.api.factory.list.primitive.ImmutableIntListFactory;
+import org.eclipse.collections.api.factory.list.primitive.ImmutableLongListFactory;
 import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.api.list.primitive.ImmutableIntList;
+import org.eclipse.collections.api.list.primitive.ImmutableLongList;
+import org.eclipse.collections.impl.list.immutable.ImmutableListFactoryImpl;
+import org.eclipse.collections.impl.list.immutable.primitive.ImmutableIntListFactoryImpl;
+import org.eclipse.collections.impl.list.immutable.primitive.ImmutableLongListFactoryImpl;
 
 import io.ffreedom.common.utils.ArrayUtil;
 
@@ -14,25 +18,52 @@ public final class ImmutableLists {
 	}
 
 	/**
+	 * 
+	 * immutable int list
+	 */
+	public static ImmutableIntListFactory immutableIntListFactory() {
+		return ImmutableIntListFactoryImpl.INSTANCE;
+	}
+
+	public static ImmutableIntList newImmutableIntList(int... values) {
+		if (ArrayUtil.isNullOrEmpty(values))
+			return ImmutableIntListFactoryImpl.INSTANCE.empty();
+		return ImmutableIntListFactoryImpl.INSTANCE.with(values);
+	}
+
+	/**
+	 * 
+	 * immutable long list
+	 */
+	public static ImmutableLongListFactory immutableLongListFactory() {
+		return ImmutableLongListFactoryImpl.INSTANCE;
+	}
+
+	public static ImmutableLongList newImmutableLongList(long... values) {
+		if (ArrayUtil.isNullOrEmpty(values))
+			return ImmutableLongListFactoryImpl.INSTANCE.empty();
+		return ImmutableLongListFactoryImpl.INSTANCE.with(values);
+	}
+
+	/**
+	 * 
 	 * immutable list
 	 */
-	public static <E> ImmutableList<E> newImmutableList(Supplier<Iterable<E>> supplier) {
-		if (supplier == null)
-			return INSTANCE.empty();
-		return newImmutableList(supplier.get());
+	public static ImmutableListFactory immutableListFactory() {
+		return ImmutableListFactoryImpl.INSTANCE;
 	}
 
 	public static <E> ImmutableList<E> newImmutableList(Iterable<E> iterable) {
 		if (iterable == null)
-			return INSTANCE.empty();
-		return INSTANCE.withAll(iterable);
+			return ImmutableListFactoryImpl.INSTANCE.empty();
+		return ImmutableListFactoryImpl.INSTANCE.withAll(iterable);
 	}
 
 	@SafeVarargs
 	public static <E> ImmutableList<E> newImmutableList(E... values) {
 		if (ArrayUtil.isNullOrEmpty(values))
-			return INSTANCE.empty();
-		return INSTANCE.with(values);
+			return ImmutableListFactoryImpl.INSTANCE.empty();
+		return ImmutableListFactoryImpl.INSTANCE.with(values);
 	}
 
 }
