fd0567159edd8ba3edcb5ab873c91ba9a2f20d2e
yellow013 <wk9988@gmail.com>
2019-12-14 17:48:02 +0800

    update StringUtil

--- commons-core/src/main/java/io/mercury/common/utils/StringUtil.java
+++ commons-core/src/main/java/io/mercury/common/utils/StringUtil.java
@@ -28,10 +28,10 @@ public final class StringUtil {
 	@Nonnull
 	public static String toString(@Nullable Object... objs) {
 		if (objs == null)
-			return "";
-		StringBuilder builder = new StringBuilder(objs.length * 2 * 16).append('[');
+			return StringConstants.EMPTY;
+		StringBuilder builder = new StringBuilder(objs.length * 20).append('[');
 		for (int i = 0, j = objs.length - 1; i < objs.length; i++) {
-			builder.append(objs[i].toString());
+			builder.append(toString(objs[i]));
 			if (i < j)
 				builder.append(',');
 		}
@@ -65,40 +65,98 @@ public final class StringUtil {
 	// TODO
 	// 改进性能,使用str.charAt(index)
 	// 不进行char数组的copy
+//	public static boolean isDecimal(String str) {
+//		// Character.isDigit(ch);
+//		if (isNullOrEmpty(str))
+//			return false;
+//		char[] chars = str.toCharArray();
+//		// 判断是否负数
+//		if (chars[0] == '-')
+//			chars[0] = '0';
+//		// 获取最后一个字符
+//		char lastChar = chars[chars.length - 1];
+//		// 判断是否为long double float的写法
+//		if (lastChar == 'L' || lastChar == 'l' || lastChar == 'D' || lastChar == 'd' || lastChar == 'F'
+//				|| lastChar == 'f')
+//			chars[chars.length - 1] = '0';
+//		// 小数点标识
+//		boolean decimalPointFlag = false;
+//		for (char ch : chars) {
+//			// 判断每个字母是否为数字
+//			if (!(ch >= '0' && ch <= '9'))
+//				// 出现第二个小数点返回false
+//				if (decimalPointFlag)
+//					return false;
+//				// 标识已出现一个小数点
+//				else if (ch == '.')
+//					decimalPointFlag = true;
+//				// 出现其他字符返回false
+//				else
+//					return false;
+//		}
+//		return true;
+//	}
+
+	/**
+	 * 检查输入参数是否为数字
+	 * 
+	 * @param str
+	 * @return
+	 */
 	public static boolean isDecimal(String str) {
-		// Character.isDigit(ch);
 		if (isNullOrEmpty(str))
 			return false;
-		char[] chars = str.toCharArray();
-		// 判断是否负数
-		if (chars[0] == '-')
-			chars[0] = '0';
-		// 获取最后一个字符
-		char lastChar = chars[chars.length - 1];
-		// 判断是否为long double float的写法
-		if (lastChar == 'L' || lastChar == 'l' || lastChar == 'D' || lastChar == 'd' || lastChar == 'F'
-				|| lastChar == 'f')
-			chars[chars.length - 1] = '0';
-		// 小数点标识
-		boolean decimalPointFlag = false;
-		for (char ch : chars) {
-			// 判断每个字母是否为数字
-			if (!(ch >= '0' && ch <= '9'))
-				// 出现第二个小数点返回false
-				if (decimalPointFlag)
-					return false;
-				// 标识已出现一个小数点
-				else if (ch == '.')
-					decimalPointFlag = true;
-				// 出现其他字符返回false
-				else
-					return false;
-		}
-		return true;
+		if (str.length() > 1) {
+			// 定义开始检查索引
+			int index = 0;
+			// 判断是否负数,如果是负数,跳过第一位的检查
+			if (str.charAt(0) == '-')
+				index = 1;
+			// 定义结束位置
+			int endPoint = str.length();
+			// 获取最后一个字符
+			char lastChar = str.charAt(str.length() - 1);
+			// 判断是否为long double float的写法
+			if (lastChar == 'L' || lastChar == 'l' || lastChar == 'D' || lastChar == 'd' || lastChar == 'F'
+					|| lastChar == 'f')
+				endPoint = str.length() - 1;
+			// 如果没有数字可以检查且第一位与最后一位都跳过了检查, 则[index == endPoint], 且输入参数绝对不是数字
+			if (index == endPoint)
+				return false;
+			// 小数点标识
+			boolean hasDecimalPoint = false;
+			for (; index < endPoint; index++) {
+				// 判断每个字母是否为数字
+				char ch = str.charAt(index);
+				if (!(ch >= '0' && ch <= '9')) {
+					// 出现第二个小数点,返回false
+					if (hasDecimalPoint)
+						return false;
+					// 标识出现了一个小数点
+					else if (ch == '.')
+						hasDecimalPoint = true;
+					// 出现其他字符返回false
+					else
+						return false;
+				}
+			}
+			return true;
+		} else
+			return str.charAt(0) >= '0' && str.charAt(0) <= '9';
 	}
 
 	public static boolean notDecimal(String str) {
 		return !isDecimal(str);
+	}
+
+	/**
+	 * 去除连接符号<b> "." </b>&<b> "-" </b>&<b> "_" </b>
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String replaceJoinChar(String str) {
+		return str == null ? StringConstants.EMPTY : str.replace(".", "").replace("-", "").replace("_", "");
 	}
 
 	public static String conversionGbkToUtf8(String gbkStr) {
@@ -278,10 +336,10 @@ public final class StringUtil {
 		System.out.println(fixPath("ddd"));
 		System.out.println(fixPath("/user/"));
 
-		System.out.println(isDecimal("877f"));
+		System.out.println(isDecimal("-l"));
 		System.out.println(isDecimal("877F"));
 		System.out.println(isDecimal("877l"));
-		System.out.println(isDecimal("877L"));
+		System.out.println(isDecimal("877S"));
 		System.out.println(isDecimal("877d"));
 		System.out.println(isDecimal("877D"));
 		System.out.println(isDecimal("-877"));
