9d973d6633c484d33a50023ef58c0a04b7585996
yellow013 <wk9988@gmail.com>
2019-12-25 20:30:26 +0800

    update byte util

--- commons-core/src/main/java/io/mercury/common/utils/BytesUtil.java
+++ commons-core/src/main/java/io/mercury/common/utils/BytesUtil.java
@@ -13,19 +13,14 @@ public final class BytesUtil {
 	public static final int bytesToInt(@Nonnull byte[] bytes) {
 		if (bytes == null || bytes.length < 4)
 			throw new ArrayIndexOutOfBoundsException("byte array length must be greater than [4]");
-		return ((bytes[0] & 0xFF) << 24) 
-				| ((bytes[1] & 0xFF) << 16) 
-				| ((bytes[2] & 0xFF) << 8) 
-				| ((bytes[3] & 0xFF));
+		return ((bytes[0] & 0xFF) << 24) | ((bytes[1] & 0xFF) << 16) | ((bytes[2] & 0xFF) << 8) | ((bytes[3] & 0xFF));
 	}
 
 	@MayThrowsRuntimeException(ArrayIndexOutOfBoundsException.class)
 	public static final int bytesToInt(@Nonnull byte[] bytes, int offset) {
 		if (bytes == null || bytes.length < offset + 4)
 			throw new ArrayIndexOutOfBoundsException("byte array length must be greater than [offset + 4]");
-		return ((bytes[offset] & 0xFF) << 24) 
-				| ((bytes[offset + 1] & 0xFF) << 16) 
-				| ((bytes[offset + 2] & 0xFF) << 8)
+		return ((bytes[offset] & 0xFF) << 24) | ((bytes[offset + 1] & 0xFF) << 16) | ((bytes[offset + 2] & 0xFF) << 8)
 				| ((bytes[offset + 3] & 0xFF));
 	}
 
@@ -33,26 +28,35 @@ public final class BytesUtil {
 	public static final char bytesToChar(@Nonnull byte[] bytes) {
 		if (bytes == null || bytes.length < 2)
 			throw new ArrayIndexOutOfBoundsException("byte array length must be greater than [2]");
-		return (char) (((bytes[0] & 0xFF) << 8) 
-				| ((bytes[1] & 0xFF)));
+		return (char) (((bytes[0] & 0xFF) << 8) | ((bytes[1] & 0xFF)));
 	}
 
 	@MayThrowsRuntimeException(ArrayIndexOutOfBoundsException.class)
 	public static final char bytesToChar(@Nonnull byte[] bytes, int offset) {
 		if (bytes == null || bytes.length < offset + 2)
 			throw new ArrayIndexOutOfBoundsException("byte array length must be greater than [offset + 2]");
-		return (char) (((bytes[offset] & 0xFF) << 8) 
-				| ((bytes[offset + 1] & 0xFF)));
+		return (char) (((bytes[offset] & 0xFF) << 8) | ((bytes[offset + 1] & 0xFF)));
 	}
 
-	public static final long intMerge(int highPos, int lowPos) {
-		// TODO
-		return 0l;
+	public static final long mergeInt(int highPos, int lowPos) {
+		return (((long) highPos) << 32) | ((long) lowPos);
 	}
 
-	public static final int longSplitHighPos(long l) {
-		// TODO
-		return 0;
+	private static final long HighPosMask = 0xFFFF_FFFF_0000_0000L;
+
+	public static final int splitLongWithHighPos(long l) {
+		return (int) ((l & HighPosMask) >> 32);
+	}
+
+	private static final long LowPosMask = 0x0000_0000_FFFF_FFFFL;
+
+	public static final int splitLongWithLowPos(long l) {
+		return (int) (l & LowPosMask);
+	}
+
+	public static void main(String[] args) {
+
+		
 	}
 
 }
