85442db11e8fc6f95d9b2f573e856038f64e739e
yellow013 <wk9988@gmail.com>
2020-01-16 10:25:13 +0800

    update annotation

---
+++ commons-core/src/main/java/io/mercury/common/annotation/lang/InitializeFields.java
@@ -0,0 +1,11 @@
+package io.mercury.common.annotation.lang;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.FIELD, ElementType.METHOD })
+public @interface InitializeFields {
+
+	Class<?>[] value();
+	
+}
