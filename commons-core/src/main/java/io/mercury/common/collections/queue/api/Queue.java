acd5ae0c516c11b8e1652b529b31c9117b0f57bc
yellow013 <wk9988@gmail.com>
2020-02-07 19:26:09 +0800

    update queue api

--- commons-core/src/main/java/io/mercury/common/collections/queue/api/Queue.java
+++ commons-core/src/main/java/io/mercury/common/collections/queue/api/Queue.java
@@ -1,11 +1,15 @@
 package io.mercury.common.collections.queue.api;
 
 public interface Queue<E> {
-	
+
 	boolean enqueue(E e);
-	
+
+	default E poll() {
+		throw new UnsupportedOperationException("Interface [Queue] defined method [poll()] is not implement");
+	}
+
 	String name();
-	
+
 	public static enum WaitingStrategy {
 		SpinWaiting, SleepWaiting,
 	}
