28027f10c51f382e3c98462dd31c15770ea069a2
yellow013 <wk9988@gmail.com>
2020-02-08 19:07:03 +0800

    update queue api

--- commons-core/src/main/java/io/mercury/common/collections/queue/api/Queue.java
+++ commons-core/src/main/java/io/mercury/common/collections/queue/api/Queue.java
@@ -1,5 +1,7 @@
 package io.mercury.common.collections.queue.api;
 
+import java.util.function.Predicate;
+
 public interface Queue<E> {
 
 	boolean enqueue(E e);
@@ -8,10 +10,26 @@ public interface Queue<E> {
 		throw new UnsupportedOperationException("Interface [Queue] defined method [poll()] is not implement");
 	}
 
+	default boolean pollAndApply(PollFunction<E> function) {
+		throw new UnsupportedOperationException("Interface [Queue] defined method [poll()] is not implement");
+	}
+
 	String name();
 
 	public static enum WaitingStrategy {
 		SpinWaiting, SleepWaiting,
+	}
+
+	@FunctionalInterface
+	 interface PollFunction<E> extends Predicate<E> {
+
+		boolean apply(E e);
+
+		@Override
+		default boolean test(E e) {
+			return apply(e);
+		}
+
 	}
 
 }
