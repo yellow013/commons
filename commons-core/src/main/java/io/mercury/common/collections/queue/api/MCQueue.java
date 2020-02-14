aeb05b1cb04eb1f101acb9255b8cc02449c386ea
yellow013 <wk9988@gmail.com>
2020-02-14 20:06:55 +0800

    update queue api

--- commons-core/src/main/java/io/mercury/common/collections/queue/api/MCQueue.java
+++ commons-core/src/main/java/io/mercury/common/collections/queue/api/MCQueue.java
@@ -1,7 +1,20 @@
 package io.mercury.common.collections.queue.api;
 
+import javax.annotation.CheckForNull;
+
 public interface MCQueue<E> extends Queue<E> {
 
+	@CheckForNull
 	E dequeue();
+
+	@Override
+	default E poll() {
+		return dequeue();
+	}
+
+	@Override
+	default boolean pollAndApply(PollFunction<E> function) {
+		return function.apply(dequeue());
+	}
 
 }
