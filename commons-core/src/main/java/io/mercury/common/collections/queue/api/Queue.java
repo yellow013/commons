88c90a9291b83a975bcbf2c7aec0bcfbb770950a
yellow013 <wk9988@gmail.com>
2020-02-24 18:09:01 +0800

    update queue api

--- commons-core/src/main/java/io/mercury/common/collections/queue/api/Queue.java
+++ commons-core/src/main/java/io/mercury/common/collections/queue/api/Queue.java
@@ -2,10 +2,13 @@ package io.mercury.common.collections.queue.api;
 
 import java.util.function.Predicate;
 
+import javax.annotation.CheckForNull;
+
 public interface Queue<E> {
 
 	boolean enqueue(E e);
 
+	@CheckForNull
 	default E poll() {
 		throw new UnsupportedOperationException("Interface [Queue] defined method [poll] is not implement");
 	}
