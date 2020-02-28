c4b50d057c79a346cfffcccbcc335310b15bc248
yellow013 <wk9988@gmail.com>
2020-02-28 19:26:45 +0800

    add java lib file

--- commons-core/src/main/java/io/mercury/common/sys/SysProperties.java
+++ commons-core/src/main/java/io/mercury/common/sys/SysProperties.java
@@ -1,10 +1,12 @@
 package io.mercury.common.sys;
 
 import java.io.File;
+import java.lang.reflect.Field;
 
 import org.eclipse.collections.api.map.ImmutableMap;
 import org.eclipse.collections.api.map.MutableMap;
 
+import io.mercury.common.character.Separator;
 import io.mercury.common.collections.ImmutableMaps;
 import io.mercury.common.collections.MutableMaps;
 
@@ -110,7 +112,38 @@ public final class SysProperties {
 	public static final String USER_COUNTRY = System.getProperty("user.country");
 
 	public static void main(String[] args) {
-		All.forEachKeyValue((key, value) -> System.out.println(key + " -> " + value));
+		System.getProperties().entrySet()
+		.forEach(entity -> System.out.println(entity.getKey().toString() + "---" + entity.getValue().toString()));
+		
+		//All.forEachKeyValue((key, value) -> System.out.println(key + " -> " + value));
+		try {
+			addLibraryDir("11111");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.getProperties().entrySet()
+		.forEach(entity -> System.out.println(entity.getKey().toString() + "|||" + entity.getValue().toString()));
+	}
+	
+	//可添加java.library.path
+    private static void addLibraryDir(String libraryPath) throws Exception {
+		Field userPathsField = ClassLoader.class.getDeclaredField("usr_paths");
+		userPathsField.setAccessible(true);
+		String[] paths = (String[]) userPathsField.get(null);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < paths.length; i++) {
+			if (libraryPath.equals(paths[i])) {
+				continue;
+			}
+			sb.append(paths[i]).append(Separator.PATH_SEPARATOR);
+		}
+		sb.append(libraryPath);
+		System.setProperty("java.library.path", sb.toString());
+		final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
+		sysPathsField.setAccessible(true);
+		sysPathsField.set(null, null);
 	}
 
 }
