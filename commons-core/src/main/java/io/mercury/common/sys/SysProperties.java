2e211fceb548614dcbc85a43c1d3ae31630ea600
yellow013 <wk9988@gmail.com>
2020-02-28 17:10:18 +0800

    update add sys prop

--- commons-core/src/main/java/io/mercury/common/sys/SysProperties.java
+++ commons-core/src/main/java/io/mercury/common/sys/SysProperties.java
@@ -1,106 +1,116 @@
 package io.mercury.common.sys;
 
 import java.io.File;
-import java.util.Properties;
 
-public interface SysProperties {
+import org.eclipse.collections.api.map.ImmutableMap;
+import org.eclipse.collections.api.map.MutableMap;
 
-	/*
-	 * System.getProperty("os.name")
-	 */
-	String OS_NAME = System.getProperty("os.name");
+import io.mercury.common.collections.ImmutableMaps;
+import io.mercury.common.collections.MutableMaps;
 
-	/*
-	 * System.getProperty("java.home")
-	 */
-	String JAVA_HOME = System.getProperty("java.home");
-
-	/*
-	 * System.getProperty("java.class.path")
-	 */
-	String JAVA_CLASS_PATH = System.getProperty("java.class.path");
-
-	/*
-	 * System.getProperty("java.version")
-	 */
-	String JAVA_VERSION = System.getProperty("java.version");
-
-	/*
-	 * System.getProperty("java.vm.name")
-	 */
-	String JAVA_VM_NAME = System.getProperty("java.vm.name");
-
-	/*
-	 * System.getProperty("java.runtime.name")
-	 */
-	String JAVA_RUNTIME_NAME = System.getProperty("java.runtime.name");
-
-	/*
-	 * System.getProperty("java.runtime.version")
-	 */
-	String JAVA_RUNTIME_VERSION = System.getProperty("java.runtime.version");
-
-	/*
-	 * System.getProperty("java.io.tmpdir")
-	 */
-	String JAVA_IO_TMPDIR = System.getProperty("java.io.tmpdir");
-
-	/**
-	 * tmpdir file
-	 */
-	File JAVA_IO_TMPDIR_FILE = new File(JAVA_IO_TMPDIR + "/");
-
-	/*
-	 * System.getProperty("user.name")
-	 */
-	String USER_NAME = System.getProperty("user.name");
-
-	/*
-	 * System.getProperty("user.home")
-	 */
-	String USER_HOME = System.getProperty("user.home");
-
-	/*
-	 * user.home file
-	 */
-	File USER_HOME_FILE = new File(USER_HOME + "/");
-
-	/*
-	 * System.getProperty("user.dir")
-	 */
-	String USER_DIR = System.getProperty("user.dir");
-
-	/**
-	 * user.dir file
-	 */
-	File USER_DIR_FILE = new File(USER_DIR + "/");
-
-	/*
-	 * System.getProperty("user.timezone")
-	 */
-	String USER_TIMEZONE = System.getProperty("user.timezone");
-
-	/*
-	 * System.getProperty("user.language")
-	 */
-	String USER_LANGUAGE = System.getProperty("user.language");
-
-	/*
-	 * System.getProperty("user.country")
-	 */
-	String USER_COUNTRY = System.getProperty("user.country");
+public final class SysProperties {
 
 	/**
 	 * System.getProperties()
 	 */
-	Properties All = System.getProperties();
+	public static final ImmutableMap<String, String> All = ImmutableMaps.newMap(() -> {
+		MutableMap<String, String> map = MutableMaps.newUnifiedMap();
+		System.getProperties().entrySet()
+				.forEach(entity -> map.put(entity.getKey().toString(), entity.getValue().toString()));
+		return map;
+	});
 
-	public static String get(String key) {
-		return All.getProperty(key);
+	public static final String get(String key) {
+		return All.get(key);
 	}
 
+	/*
+	 * System.getProperty("os.name")
+	 */
+	public static final String OS_NAME = System.getProperty("os.name");
+
+	/*
+	 * System.getProperty("java.home")
+	 */
+	public static final String JAVA_HOME = System.getProperty("java.home");
+
+	/*
+	 * System.getProperty("java.class.path")
+	 */
+	public static final String JAVA_CLASS_PATH = System.getProperty("java.class.path");
+
+	/*
+	 * System.getProperty("java.version")
+	 */
+	public static final String JAVA_VERSION = System.getProperty("java.version");
+
+	/*
+	 * System.getProperty("java.vm.name")
+	 */
+	public static final String JAVA_VM_NAME = System.getProperty("java.vm.name");
+
+	/*
+	 * System.getProperty("java.runtime.name")
+	 */
+	public static final String JAVA_RUNTIME_NAME = System.getProperty("java.runtime.name");
+
+	/*
+	 * System.getProperty("java.runtime.version")
+	 */
+	public static final String JAVA_RUNTIME_VERSION = System.getProperty("java.runtime.version");
+
+	/*
+	 * System.getProperty("java.io.tmpdir")
+	 */
+	public static final String JAVA_IO_TMPDIR = System.getProperty("java.io.tmpdir");
+
+	/**
+	 * tmpdir file
+	 */
+	public static final File JAVA_IO_TMPDIR_FILE = new File(JAVA_IO_TMPDIR + "/");
+
+	/*
+	 * System.getProperty("user.name")
+	 */
+	public static final String USER_NAME = System.getProperty("user.name");
+
+	/*
+	 * System.getProperty("user.home")
+	 */
+	public static final String USER_HOME = System.getProperty("user.home");
+
+	/*
+	 * user.home file
+	 */
+	public static final File USER_HOME_FILE = new File(USER_HOME + "/");
+
+	/*
+	 * System.getProperty("user.dir")
+	 */
+	public static final String USER_DIR = System.getProperty("user.dir");
+
+	/**
+	 * user.dir file
+	 */
+	public static final File USER_DIR_FILE = new File(USER_DIR + "/");
+
+	/*
+	 * System.getProperty("user.timezone")
+	 */
+	public static final String USER_TIMEZONE = System.getProperty("user.timezone");
+
+	/*
+	 * System.getProperty("user.language")
+	 */
+	public static final String USER_LANGUAGE = System.getProperty("user.language");
+
+	/*
+	 * System.getProperty("user.country")
+	 */
+	public static final String USER_COUNTRY = System.getProperty("user.country");
+
 	public static void main(String[] args) {
-		All.entrySet().forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));
+		All.forEachKeyValue((key, value) -> System.out.println(key + " -> " + value));
 	}
 
 }
