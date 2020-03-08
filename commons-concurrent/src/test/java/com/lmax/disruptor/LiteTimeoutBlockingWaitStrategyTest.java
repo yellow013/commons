aa9d6760a6c70157f6e6ccb1f15e76a7ccaec0a7
yellow013 <wk9988@gmail.com>
2020-03-08 18:47:03 +0800

    disruptor test case

---
+++ commons-concurrent/src/test/java/com/lmax/disruptor/LiteTimeoutBlockingWaitStrategyTest.java
@@ -0,0 +1,37 @@
+package com.lmax.disruptor;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import com.lmax.disruptor.support.DummySequenceBarrier;
+
+public class LiteTimeoutBlockingWaitStrategyTest {
+	@Test
+	public void shouldTimeoutWaitFor() throws Exception {
+		final SequenceBarrier sequenceBarrier = new DummySequenceBarrier();
+
+		long theTimeout = 500;
+		LiteTimeoutBlockingWaitStrategy waitStrategy = new LiteTimeoutBlockingWaitStrategy(theTimeout,
+				TimeUnit.MILLISECONDS);
+		Sequence cursor = new Sequence(5);
+		Sequence dependent = cursor;
+
+		long t0 = System.currentTimeMillis();
+
+		try {
+			waitStrategy.waitFor(6, cursor, dependent, sequenceBarrier);
+			fail("TimeoutException should have been thrown");
+		} catch (TimeoutException e) {
+		}
+
+		long t1 = System.currentTimeMillis();
+
+		long timeWaiting = t1 - t0;
+
+		assertTrue(timeWaiting >= theTimeout);
+	}
+}
