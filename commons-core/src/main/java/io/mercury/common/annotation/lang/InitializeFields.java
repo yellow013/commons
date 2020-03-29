61e37b9aa31dad39ec504085f64a3d20038a0069
yellow013 <wk9988@gmail.com>
2020-03-29 22:13:32 +0800

    update file

--- commons-core/src/main/java/io/mercury/common/annotation/lang/InitializeFields.java
+++ commons-core/src/main/java/io/mercury/common/annotation/lang/InitializeFields.java
@@ -7,5 +7,5 @@ import java.lang.annotation.Target;
 public @interface InitializeFields {
 
 	Class<?>[] value();
-	
+
 }
--- commons-core/src/main/java/io/mercury/common/annotation/lang/NativeObject.java
+++ commons-core/src/main/java/io/mercury/common/annotation/lang/NativeObject.java
@@ -4,7 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @Target({ ElementType.FIELD })
-public @interface JNI {
+public @interface NativeObject {
 
 	String[] headFile() default "";
 
