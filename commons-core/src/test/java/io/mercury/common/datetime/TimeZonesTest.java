95d5c900fe052a8d1e4d296e53192ad410afe09d
yellow013 <wk9988@gmail.com>
2020-03-31 23:04:59 +0800

    update test time zone

--- commons-core/src/test/java/io/mercury/common/datetime/TimeZonesTest.java
+++ commons-core/src/test/java/io/mercury/common/datetime/TimeZonesTest.java
@@ -6,35 +6,27 @@ import java.time.ZoneOffset;
 
 import org.junit.Test;
 
-import io.mercury.common.datetime.TimeZone;
-
 public class TimeZonesTest {
 
 	@Test
 	public void test() {
-		
+
 		ZoneOffset standardOffset = ZoneId.systemDefault().getRules().getStandardOffset(Instant.EPOCH);
-		
+
 		System.out.println(standardOffset);
-		
+
 		System.out.println(TimeZone.SYS_DEFAULT_OFFSET);
 		System.out.println(TimeZone.CST_OFFSET);
 		System.out.println(TimeZone.JST_OFFSET);
-		
+
 		ZoneOffset ofHours = ZoneOffset.ofHours(8);
 
 		System.out.println(TimeZone.SYS_DEFAULT_OFFSET.equals(ofHours));
-		
+
 		System.out.println(TimeZone.SYS_DEFAULT_OFFSET.equals(standardOffset));
 
-		ZoneId.getAvailableZoneIds().forEach(this::println);
-		
-		
+		ZoneId.getAvailableZoneIds().forEach(System.out::println);
 
-	}
-
-	private void println(String str) {
-		System.out.println(str);
 	}
 
 }
