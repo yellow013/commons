c66f188a1963aefe2a6472b2ecffb09a6a36d2d1
yellow013 <wk9988@gmail.com>
2020-06-04 00:40:47 +0800

    update common

---
+++ commons-core/src/main/java/io/mercury/common/number/DecimalSupporter.java
@@ -0,0 +1,140 @@
+package io.mercury.common.number;
+
+/**
+ * 
+ * @author yellow013
+ *
+ */
+public final class DecimalSupporter {
+
+	/**
+	 * 
+	 */
+	public static final long LONG_MULTIPLIER_1 = 1L;
+
+	/**
+	 * 
+	 */
+	public static final double DOUBLE_MULTIPLIER_1 = 1.0D;
+
+	/**
+	 * 
+	 */
+	public static final long LONG_MULTIPLIER_100 = 100L;
+
+	/**
+	 * 
+	 */
+	public static final double DOUBLE_MULTIPLIER_100 = 100.0D;
+
+	/**
+	 * 
+	 */
+	public static final long LONG_MULTIPLIER_10000 = 10_000L;
+
+	/**
+	 * 
+	 */
+	public static final double DOUBLE_MULTIPLIER_10000 = 10_000.0D;
+
+	/**
+	 * 
+	 */
+	public static final long LONG_MULTIPLIER_1000000 = 1_000_000L;
+
+	/**
+	 * 
+	 */
+	public static final double DOUBLE_MULTIPLIER_1000000 = 1_000_000.0D;
+
+	/**
+	 * 
+	 */
+	public static final long LONG_MULTIPLIER_100000000 = 100_000_000L;
+
+	/**
+	 * 
+	 */
+	public static final double DOUBLE_MULTIPLIER_100000000 = 100_000_000.0D;
+
+	/**
+	 * 
+	 * @param price
+	 * @return
+	 */
+	public final static long doubleToLong2(double d) {
+		return (long) (d * LONG_MULTIPLIER_100);
+	}
+
+	/**
+	 * 
+	 * @param price
+	 * @return
+	 */
+	public final static double longToDouble2(long l) {
+		return l / DOUBLE_MULTIPLIER_100;
+	}
+
+	/**
+	 * 
+	 * @param price
+	 * @return
+	 */
+	public final static long doubleToLong4(double d) {
+		return (long) (d * LONG_MULTIPLIER_10000);
+	}
+
+	/**
+	 * 
+	 * @param price
+	 * @return
+	 */
+	public final static double longToDouble4(long l) {
+		return l / DOUBLE_MULTIPLIER_10000;
+	}
+
+	/**
+	 * 
+	 * @param price
+	 * @return
+	 */
+	public final static long doubleToLong6(double d) {
+		return (long) (d * LONG_MULTIPLIER_1000000);
+	}
+
+	/**
+	 * 
+	 * @param price
+	 * @return
+	 */
+	public final static double longToDouble6(long l) {
+		return l / DOUBLE_MULTIPLIER_1000000;
+	}
+
+	/**
+	 * 
+	 * @param price
+	 * @return
+	 */
+	public final static long doubleToLong8(double d) {
+		return (long) (d * LONG_MULTIPLIER_100000000);
+	}
+
+	/**
+	 * 
+	 * @param price
+	 * @return
+	 */
+	public final static double longToDouble8(long l) {
+		return l / DOUBLE_MULTIPLIER_100000000;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println();
+
+		System.out.println(longToDouble8(doubleToLong8(4.981312)));
+
+	}
+
+}
